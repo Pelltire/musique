764644747800f96a8f7c1b0752615be629361940
Pelltire <73190861+Pelltire@users.noreply.github.com>
2020-11-17 14:04:27 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,40 @@
+for (let index = 0; index < 2; index++) {
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(147, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 2; index++) {
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 2; index++) {
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(165, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 2; index++) {
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(147, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Whole))
+}
+basic.forever(function () {
+	
+})
